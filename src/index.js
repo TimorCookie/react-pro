d4d286a08f9b36fa3d8726dff00c23017620859e
timokie <xiaoshame@126.com>
2019-02-18 14:56:08 +0800

    uptimize structure

--- src/index.js
+++ src/index.js
@@ -1,66 +1,16 @@
-import { createStore } from 'redux';
-import { combineReducers } from 'redux';
+import store from './store';
+import {
+  addToCart,
+  updatedCart,
+  deleteFromCart
+} from './actions/cart-actions';
 
-const allReducers = {
-  products: productsReducer,
-  shoppingCart: cartReducer
-}
+console.log("initial state:", store.getState());
 
+let unsubscribe = store.subscribe(() => console.log(store.getState()));
+store.dispatch(addToCart('apple', 2, 40));
+store.dispatch(addToCart('banana', 1, 32));
+store.dispatch(updatedCart('bread 700g',20,500))
+store.dispatch(deleteFromCart('banana'));
 
-const initialState = {
-  cart: [
-    {
-      product: 'bread 700g',
-      quantity: 2,
-      unitCost: 90
-    },
-    {
-      product: 'milk 500ml',
-      quantity: 1,
-      unitCost: 47
-    }
-  ]
-}
-const ADD_TO_CART = 'ADD_TO_CART'
-
-
-function productsReducer (state=[], action) {
-  return state
-}
-function cartReducer (state=initialState, action) {
-  switch(action.type) {
-    case ADD_TO_CART: {
-      return {
-        ...state,
-        cart: [...state.cart, action.payload]
-      }
-    }
-
-    default: 
-    return state
-  }
-  return state
-}
-
-
-function addToCart(product, quantity, unitCost) {
-  return {
-    type: ADD_TO_CART,
-    payload: {
-      product, quantity, unitCost
-    }
-  }
-}
-
-
-
-const rootReducer = combineReducers(allReducers)
-const store = createStore(rootReducer)
-console.log('initial state:', store.getState())
-
-let unsubscribe = store.subscribe(()=> console.log(store.getState()))
-
-store.dispatch(addToCart('orange 1kg',1,47))
-store.dispatch(addToCart('apple 2kg',3,20))
 unsubscribe()
-
